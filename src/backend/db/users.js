8d338c9b983a9dfee08551fba7a544896e867727
Prachi Sahani <prachirsahani97@gmail.com>
2022-05-21 17:46:26 +0530

    initial backend setup

---
+++ src/backend/db/users.js
@@ -0,0 +1,28 @@
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
+
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * */
+
+export const users = [
+  {
+    _id: uuid(),
+    firstName: "Adarsh",
+    lastName: "Balika",
+    username: "adarshbalika",
+    password: "adarshBalika123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    firstName: "Shubham",
+    lastName: "Soni",
+    username: "shubhamsoni",
+    password: "shubhamSoni123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+];
