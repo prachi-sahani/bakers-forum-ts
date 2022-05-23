6dca8dcfecdec542373b566ef668c25e23f57c50
Prachi Sahani <prachirsahani97@gmail.com>
2022-05-23 21:47:14 +0530

    feat - custom snackbar

---
+++ src/components/CustomSnackbar.tsx
@@ -0,0 +1,30 @@
+import {
+  Snackbar,
+  IconButton,
+} from "../utilities/material-ui/material-components";
+import { CloseIcon } from "../utilities/material-ui/material-icons";
+import React from "react";
+export function CustomSnackbar({ message }: { message: string }) {
+  const [open, setOpen] = React.useState<boolean>(true);
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      open={open}
+      autoHideDuration={3000}
+      onClose={() => setOpen(false)}
+      message={message}
+      action={
+        <React.Fragment>
+          <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={() => setOpen(false)}
+          >
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        </React.Fragment>
+      }
+    />
+  );
+}
