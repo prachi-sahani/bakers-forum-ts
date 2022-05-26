6ac2a02307bda596bf4d05a0186c91454abc118f
Prachi Sahani <prachirsahani97@gmail.com>
2022-05-26 18:59:45 +0530

    refactor - updated backend data

--- src/backend/db/questions.js
+++ src/backend/db/questions.js
@@ -8,30 +8,79 @@ import { formatDate } from "../utils/authUtils";
 export const questions = [
   {
     _id: uuid(),
-    username: "adarshbalika",
-    questionTitle: "Why to use Server Side Rendering",
-    questionText: "I am Detailed Description about the Question.",
+    username: "prachisahani",
+    questionTitle: "Hi everyone!!",
+    questionText: "I'm really excited to be here.",
     votes: {
-      upvotedBy: [],
+      upvotedBy: ["zuberdunge"],
       downvotedBy: [],
     },
     comments: [
       {
         _id: uuid(),
-        username: "shubhamsoni",
-        firstName: "Shubham",
-        lastName:"Soni",
-        commentText: "Interesting",
+        username: "itsHamhere",
+        firstName: "Hamza",
+        lastName: "Husein",
+        commentText: "Welcome to the forum, Prachi!",
       },
       {
         _id: uuid(),
-        username: "sohamshah",
-        firstName: "Soham",
-        lastName:"Shah",
-        commentText: "Wow!",
+        username: "hinanshi04",
+        firstName: "Hinanshi",
+        lastName: "Suthar",
+        commentText: "Hi!",
       },
     ],
-    tags:["Baking", "Cakes"],
+    tags: [],
+    answers: [
+      {
+        _id: uuid(),
+        username: "sohamshah",
+        answerText:
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+        votes: {
+          upvotedBy: [],
+          downvotedBy: [],
+        },
+        comments: [
+          {
+            _id: uuid(),
+            username: "shubhamsoni",
+            commentText: "Thanks for the answer!",
+          },
+        ],
+      },
+    ],
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    username: "zuberdunge",
+    questionTitle: "Hey, hey, hey!!",
+    questionText:
+      "Hello I am new here. I like fruit breads. Banana bread is my favorite.",
+    votes: {
+      upvotedBy: ["jerry", "itsHamhere"],
+      downvotedBy: [],
+    },
+    comments: [
+      {
+        _id: uuid(),
+        username: "jerryBoleMeow",
+        firstName: "Jerry",
+        lastName: "Dunge",
+        commentText: "I love fruit breads too.",
+      },
+      {
+        _id: uuid(),
+        username: "prachisahani",
+        firstName: "Prachi",
+        lastName: "Sahani",
+        commentText: "Welcome Zubar!",
+      },
+    ],
+    tags: [],
     answers: [
       {
         _id: uuid(),
