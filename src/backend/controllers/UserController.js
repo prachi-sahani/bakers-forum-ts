56b1474d38315dd0a00eb7364cee872951235aed
Prachi Sahani <prachirsahani97@gmail.com>
2022-05-26 12:22:01 +0530

    refactor - updated backend files

--- src/backend/controllers/UserController.js
+++ src/backend/controllers/UserController.js
@@ -1,5 +1,5 @@
 import { Response } from "miragejs";
-import { requiresAuth } from "../utils/authUtils";
+import { formatDate, requiresAuth } from "../utils/authUtils";
 
 /**
  * All the routes related to user are present here.
@@ -59,6 +59,132 @@ export const editUserHandler = function (schema, request) {
     user = { ...user, ...userData };
     this.db.users.update({ _id: user._id }, user);
     return new Response(201, {}, { user });
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
+
+/**
+ * This handler handles follow action.
+ * send POST Request at /api/users/follow/:followUserId/
+ * */
+
+export const followUserHandler = function (schema, request) {
+  const user = requiresAuth.call(this, request);
+  const { followUserId } = request.params;
+  const followUser = schema.users.findBy({ _id: followUserId }).attrs;
+  try {
+    if (!user) {
+      return new Response(
+        404,
+        {},
+        {
+          errors: [
+            "The username you entered is not Registered. Not Found error",
+          ],
+        }
+      );
+    }
+    const isFollowing = user.following.some(
+      (currUser) => currUser._id === followUser._id
+    );
+
+    if (isFollowing) {
+      return new Response(400, {}, { errors: ["User Already following"] });
+    }
+
+    const updatedUser = {
+      ...user,
+      following: [...user.following, { ...followUser }],
+    };
+    const updatedFollowUser = {
+      ...followUser,
+      followers: [...followUser.followers, { ...user }],
+    };
+    this.db.users.update(
+      { _id: user._id },
+      { ...updatedUser, updatedAt: formatDate() }
+    );
+    this.db.users.update(
+      { _id: followUser._id },
+      { ...updatedFollowUser, updatedAt: formatDate() }
+    );
+    return new Response(
+      200,
+      {},
+      { user: updatedUser, followUser: updatedFollowUser }
+    );
+  } catch (error) {
+    return new Response(
+      500,
+      {},
+      {
+        error,
+      }
+    );
+  }
+};
+
+/**
+ * This handler handles unfollow action.
+ * send POST Request at /api/users/unfollow/:followUserId/
+ * */
+
+export const unfollowUserHandler = function (schema, request) {
+  const user = requiresAuth.call(this, request);
+  const { followUserId } = request.params;
+  const followUser = this.db.users.findBy({ _id: followUserId });
+  try {
+    if (!user) {
+      return new Response(
+        404,
+        {},
+        {
+          errors: [
+            "The username you entered is not Registered. Not Found error",
+          ],
+        }
+      );
+    }
+    const isFollowing = user.following.some(
+      (currUser) => currUser._id === followUser._id
+    );
+
+    if (!isFollowing) {
+      return new Response(400, {}, { errors: ["User already not following"] });
+    }
+
+    const updatedUser = {
+      ...user,
+      following: user.following.filter(
+        (currUser) => currUser._id !== followUser._id
+      ),
+    };
+    const updatedFollowUser = {
+      ...followUser,
+      followers: followUser.followers.filter(
+        (currUser) => currUser._id !== user._id
+      ),
+    };
+    this.db.users.update(
+      { _id: user._id },
+      { ...updatedUser, updatedAt: formatDate() }
+    );
+    this.db.users.update(
+      { _id: followUser._id },
+      { ...updatedFollowUser, updatedAt: formatDate() }
+    );
+    return new Response(
+      200,
+      {},
+      { user: updatedUser, followUser: updatedFollowUser }
+    );
   } catch (error) {
     return new Response(
       500,
