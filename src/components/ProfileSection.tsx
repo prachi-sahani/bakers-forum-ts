5fdb55539b24180b5f8fbf2e157d3c40d9ad6f6f
Prachi Sahani <prachirsahani97@gmail.com>
2022-05-30 21:59:56 +0530

    refactor - added error handling

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -4,11 +4,12 @@ import { getQuestions } from "../redux/slices/feedSlice";
 import { RootState } from "../redux/store";
 import { Question } from "../types/Question";
 import { Box } from "../utilities/material-ui/material-components";
+import { CustomSnackbar } from "./CustomSnackbar";
 import { QuestionCardsSection } from "./QuestionCardsSection";
 import { UserProfile } from "./UserProfile";
 
 export function ProfileSection() {
-  const { userDetails } = useAppSelector(
+  const { userDetails, editUserAPIError } = useAppSelector(
     (state: RootState) => state.authentication
   );
   const dispatch = useAppDispatch();
@@ -42,6 +43,7 @@ export function ProfileSection() {
     >
       <UserProfile user={userDetails} />
       <QuestionCardsSection title="My Posts" questions={questionsToDisplay} />
+      {editUserAPIError && <CustomSnackbar message={editUserAPIError} />}
     </Box>
   );
 }
