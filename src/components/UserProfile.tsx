1a6632e14f673614f5eaf1c27c1e0ece2860359d
Prachi Sahani <prachirsahani97@gmail.com>
2022-05-30 22:30:22 +0530

    refactor - updating following and followers count

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -8,9 +8,22 @@ import { grey } from "../utilities/material-ui/material-colors";
 import { UserDetails } from "../types/UserDetails";
 import { EditProfile } from "./EditProfile";
 import React from "react";
+import { useAppSelector } from "../redux/customHook";
+import { RootState } from "../redux/store";
 export function UserProfile({ user }: { user: UserDetails }) {
+  const { followUserAPIStatus, unfollowUserAPIStatus } = useAppSelector(
+    (state: RootState) => state.users
+  );
   const [openEditProfile, setOpenEditProfile] = React.useState(false);
   const openEditModal = () => setOpenEditProfile(true);
+  const [following, setFollowing] = React.useState(user.following);
+  const [followers, setFollowers] = React.useState(user.followers);
+
+  // update followers and following list based on actions in "you may also know section"
+  React.useEffect(() => {
+    setFollowing(JSON.parse(localStorage.getItem("user") || "{}").following);
+    setFollowers(JSON.parse(localStorage.getItem("user") || "{}").followers);
+  }, [followUserAPIStatus, unfollowUserAPIStatus]);
   return (
     <Box
       sx={{
@@ -42,12 +55,8 @@ export function UserProfile({ user }: { user: UserDetails }) {
         }}
       >
         <Typography variant="body2">23 posts</Typography>
-        <Typography variant="body2">
-          {user.followers.length} followers
-        </Typography>
-        <Typography variant="body2">
-          {user.following.length} following
-        </Typography>
+        <Typography variant="body2">{followers.length} followers</Typography>
+        <Typography variant="body2">{following.length} following</Typography>
       </Box>
       <Button
         variant="outlined"
